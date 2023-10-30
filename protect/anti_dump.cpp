f1c7dfc927c400608daafb5d5e734cfd0294f8e5
Emir <77904942+ReFo0@users.noreply.github.com>
2023-10-30 19:38:30 +0300

    Update anti_dump.cpp

--- protect/anti_dump.cpp
+++ protect/anti_dump.cpp
@@ -2,7 +2,7 @@
 #include <Windows.h>
 #include <winternl.h>
 
-int anti_dump()
+void anti_dump()
 {
 	const auto peb = (PPEB)__readgsqword(0x60);
 
@@ -10,7 +10,4 @@ int anti_dump()
 	const auto table_entry = CONTAINING_RECORD(in_load_order_module_list, LDR_DATA_TABLE_ENTRY, Reserved1[0]);
 	const auto p_size_of_image = (PULONG)&table_entry->Reserved3[1];
 	*p_size_of_image = (ULONG)((INT_PTR)table_entry->DllBase + 0x100000);
-
-	return 0;
-
 };
