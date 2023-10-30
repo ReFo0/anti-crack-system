63a7a6307c764ba4823bf7ad568f3f5d7a283e3e
Emir <77904942+ReFo0@users.noreply.github.com>
2023-10-30 19:42:15 +0300

    Update node_protect.cpp

--- protect/nodeserver/node_protect.cpp
+++ protect/nodeserver/node_protect.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <urlmon.h>
 #include "node_protect.h"
-#include "../../encryption/xor.h"
+#include "../../encryption/obstcate.h"
 #pragma comment(lib, "urlmon.lib")
 
 
@@ -10,7 +10,7 @@
 
 bool node_client()
 {
-	std::string url = _xor_("http://127.0.0.1:3000").c_str();
+	std::string url = OBFUSCATE("http://127.0.0.1:3000");
 	IStream* stream;
 	auto result = URLOpenBlockingStreamA(0, url.c_str(), &stream, 0, 0);
 	return true;
