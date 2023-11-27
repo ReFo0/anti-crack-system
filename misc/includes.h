5f6d58e79458f822eae30adc3ebcb75946b5001e
Emir <77904942+ReFo0@users.noreply.github.com>
2023-11-28 01:50:04 +0300

    Add files via upload

---
+++ misc/includes.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../protect/protectmain.h"
+#include "../protect/anti_dump.h"
+#include "../protect/anti_debugger.h"
+#include "../protect/anti_attach.h"
+#include "../protect/kill_process.h"
+#include "../protect/selfcode/selfcode.h"
+#include "../protect/nodeserver/node_protect.h"
+#include "../protect/integrity_check.h"
