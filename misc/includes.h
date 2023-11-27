32bfdfd7caf675af3d2fbc61450f11e414adc54f
Emir <77904942+ReFo0@users.noreply.github.com>
2023-11-28 01:49:54 +0300

    Delete misc/includes.h

--- misc/includes.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../protect/protectmain.h"
-#include "../protect/anti_dump.h"
-#include "../protect/anti_debugger.h"
-#include "../protect/anti_attach.h"
-#include "../protect/kill_process.h"
-#include "../protect/selfcode/selfcode.h"
-#include "../protect/nodeserver/node_protect.h"
